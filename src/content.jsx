68623b1b8dbc27c613f5eba9453061b1d6fdc93a
invisibleloop <invisibleloop@users.noreply.github.com>
2019-06-30 16:52:59 +0100

    added fidler to copyright

--- src/content.jsx
+++ src/content.jsx
@@ -1,5 +1,5 @@
 export default {
   title: 'Lorem Fidsum placeholder text generator',
   description: 'The ultimate placeholder text generator from the amusing, creative and damn right strange Slack chats by the bearded one, James Fidler. Obviously, these chats are in no particular order and out of context.',
-  copyright: 'Copyright © <year> invisibleloop',
+  copyright: 'Copyright © <year> invisibleloop & James Fidler',
 };
